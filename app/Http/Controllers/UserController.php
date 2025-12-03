0c763e4e70014b0fd3668eaf650755f6869717bb
mosabAbozraa <mosababozra3@gmail.com>
2025-12-03 20:13:41 +0300

    finished register func

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,35 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function regiter(Request $request){
-        $request->validate([
-            'phone' => 'unique:users,phone'
-        ]);
-        $user = User::create([
-            'first_name' => $request->first_name,
-            'last_name'  => $request->last_name,
-            'phone'      => $request->phone,
-            'password'   => Hash::make($request->password),
-            'date_of_birth' => $request->date_of_birth
-        ]);
+    public function register(RegisterRequest $request){
+        $validated = $request->validated();
+        $validated['password'] = Hash::make($request->password);
+        if($request->hasFile('avatar')){
+            $path = $request->file('avatar')->store('avatars','public');
+            $validated['avatar'] = $path;
+        }
+        if($request->hasFile('id_photo')){
+            $path = $request->file('id_photo')->store('','id_photos');
+            $validated['id_photo'] = $path;
+        }   
+        $user = User::create($validated);
         return response()->json($user,201);
     }
 
     public function login(Request $request){
 
-    }
-
-    public function loginNormalUser(Request $request){
-
-    }
-
-    public function loginAdminUser(Request $request){
-        echo "admin login";
     }
 }
