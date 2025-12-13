31abaf05a76421e7a0d71fe12873ac187d0d54a2
mosabAbozraa <mosababozra3@gmail.com>
2025-12-14 00:04:29 +0300

    added all validation to property request

--- app/Http/Requests/AddPropertyRequest.php
+++ app/Http/Requests/AddPropertyRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Exists;
 
 class AddPropertyRequest extends FormRequest
 {
@@ -21,8 +23,21 @@ class AddPropertyRequest extends FormRequest
      */
     public function rules(): array
     {
-        return [
-            
+        return [  
+            'images'            => 'required|array',
+            'images.*'          => 'image|mimes:jpg,png,jpeg|max:2048',  // 2 MB
+            'governorate_id'    => 'required|integer|exists:governorates,id',
+            'city_id'           => [
+                'required',
+                'integer',
+                Rule::exists('cities','id')->where(function ($q){
+                    $q->where('governorate_id',$this->governorate_id);
+                })],
+            'price_per_night'   => 'required|numeric',
+            'description'       => 'sometimes|max:300',
+            'area'              => 'required|numeric',
+            'rooms'             => 'required|integer|min:1|max:20',
+            'bath_rooms'        => 'required|integer|min:0|max:20'
         ];
     }
 }
