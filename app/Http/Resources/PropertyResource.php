e1e34eb7181f1e5ab9b4d5174cad568df97ef8cc
mosabAbozraa <mosababozra3@gmail.com>
2025-12-17 23:43:55 +0300

    made resource for owner and property print the required inof for both

---
+++ app/Http/Resources/PropertyResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\City;
+use App\Models\Governorate;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PropertyResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            // owner 
+            'governorate'       => Governorate::find($this->governorate_id)->name,
+            'city'              => City::find($this->city_id)->name,
+            'price per night'   => $this->price_per_night,
+            'description'       => $this->description,
+            'area'              => $this->area,
+            'rooms'             => $this->rooms,
+            'bath rooms'        => $this->bath_rooms,
+            'date'              => $this->created_at->format('Y-m-d'),
+            'owner information' => new OwnerResource($this->whenLoaded('owner'))
+        ];
+    }
+}
