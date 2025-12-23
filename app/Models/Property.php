5556db498e80f62c9d85b0a384eae7e489425a65
mosabAbozraa <mosababozra3@gmail.com>
2025-12-23 10:20:25 +0300

    added filter search logic

--- app/Models/Property.php
+++ app/Models/Property.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use GuzzleHttp\Psr7\Query;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
 
 class Property extends Model
 {
@@ -18,6 +20,28 @@ class Property extends Model
 
     public function images(){
         return $this->hasMany(Media::class);
+    }
+
+    public function filter_search($query ,array $filters){
+        if(isset($filters['governorate_id'])){
+            $query->where('governorate_id', $filters['governorate_id']);
+        }
+        if(isset($filters['city_id'])){
+            $query->where('city_id', $filters['city_id']);
+        }
+        if(isset($filters['min_price'])){
+            $query->where('price','>=',$filters['min_price']);
+        }
+        if(isset($filters['max_price'])){
+            $query->where('price','<=',$filters['max_price']);
+        }
+        if(isset($filters['rooms'])){
+            $query->where('rooms',$filters['rooms']);
+        }
+        if(isset($filters['bath_rooms'])){
+            $query->where('bath_rooms',$filters['bath_rooms']);
+        }
+        return $query;   
     }
 
 }
