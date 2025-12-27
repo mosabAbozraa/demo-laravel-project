99a0dc98d421a682740226985317dbaa0e1fe47f
Mosab <mosababozra3@gmail.com>
2025-12-27 03:14:58 +0300

    organized api file and added everything into it

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,6 @@
 use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\UserController;
-use App\Models\Property;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,21 +17,24 @@ Route::post('logout',[UserController::class,'logout'])->middleware('auth:sanctum
 
 Route::middleware('auth:sanctum')->group(function(){
     Route::group(['prefix'=>'properties'],function(){
-        Route::post('add',[PropertyController::class,'add_property_to_owner']);  
-        Route::get('showAll',[PropertyController::class,'show_all_properties']);    
+        Route::post('add',[PropertyController::class,'add_property_to_owner']);
+        Route::get('showAll',[PropertyController::class,'show_all_properties']);
         Route::get('showOne/{propertyId}',[PropertyController::class,'getProperty']);
         Route::get('filterSearch',[PropertyController::class,'search']);
         Route::post('rate/{propertyId}',[PropertyController::class,'rate_property']);
-        //=================================== Reservations ===================================
-        Route::post('book/{propertyId}',[ReservationController::class,'booking']);
     });
-    
+    //=================================== Reservations ===================================
+    Route::post('book/{propertyId}',[ReservationController::class,'booking']);
+    Route::put('editBooking/{bookingId}',[ReservationController::class,'edit_booking']);
+    Route::post('cancelBooking/{bookingId}',[ReservationController::class,'cancel_booking']);
+    Route::get('getTenantBookings',[ReservationController::class,'tenant_bookings']);
+
     //=================================== Admin Routes ===================================
-    Route::middleware('CheckUser')->group(['prefix'=>'admin'],function(){
-        Route::get('showAllPendingUser',[UserController::class,'pendingUser']);
-        Route::post('updateUserStatus/{user_id}',[UserController::class,'updateUserStatus']);
-        Route::post('editRole/{user_id}',[UserController::class,'editRole']);
+    Route::middleware('CheckUser')->group(function(){
+        Route::group(['prefix'=>'admin'],function(){
+            Route::get('showAllPendingUser',[UserController::class,'pendingUser']);
+            Route::post('updateUserStatus/{user_id}',[UserController::class,'updateUserStatus']);
+            Route::post('editRole/{user_id}',[UserController::class,'editRole']);
+        });
     });
 });
-
-
