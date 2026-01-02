1cea3cde7aaba94781567ee9eaa1e05cf68c4754
Habib Badawi <badawi.hab99@gmail.com>
2026-01-02 20:18:01 +0300

    fix governorate name and city for qusay

--- app/Http/Requests/AddPropertyRequest.php
+++ app/Http/Requests/AddPropertyRequest.php
@@ -4,40 +4,41 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
-use Illuminate\Validation\Rules\Exists;
+use App\Models\Governorate; // 1. تأكد من استدعاء الموديل هنا
 
 class AddPropertyRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            'images'            => 'required|array',//add required here
-            'images.*'          => 'image|mimes:jpg,png,jpeg|max:5120',  // 2 MB
-            'governorate_id'    => 'required|integer|exists:governorates,id',
-            'city_id'           => [
+            'images'          => 'required|array',
+            'images.*'        => 'image|mimes:jpg,png,jpeg|max:5120',
+            'governorate'     => 'required|string|exists:governorates,name',
+            'city'            => [
                 'required',
-                'integer',
-                Rule::exists('cities','id')->where(function ($q){
-                    $q->where('governorate_id',$this->governorate_id);
-                })],
-            'price_per_night'   => 'required|numeric',
-            'description'       => 'sometimes|max:300',
-            'area'              => 'required|numeric',
-            'rooms'             => 'required|integer|min:1|max:20',
-            'bath_rooms'        => 'required|integer|min:0|max:20'
+                'string',
+                Rule::exists('cities', 'name')->where(function ($query) {
+                    
+                    $governorateName = $this->governorate;
+                    $governorate = Governorate::where('name', $governorateName)->first();
+
+                    if ($governorate) {
+                        return $query->where('governorate_id', $governorate->id);
+                    }
+                    return $query->where('id', 0);//شرط مستحيل مشان اذا فشلت المحافظة ما يرجع ولا مدينة
+                }),
+            ],
+
+            'price_per_night' => 'required|numeric',
+            'description'     => 'sometimes|max:300',
+            'area'            => 'required|numeric',
+            'rooms'           => 'required|integer|min:1|max:20',
+            'bath_rooms'      => 'required|integer|min:0|max:20'
         ];
     }
 }
