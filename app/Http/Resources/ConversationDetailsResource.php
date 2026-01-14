905e1f7b5cc81b394478f96a6ac3d4d65e8025a4
Habib Badawi <badawi.hab99@gmail.com>
2026-01-14 22:28:08 +0300

    add phone to resource

--- app/Http/Resources/ConversationDetailsResource.php
+++ app/Http/Resources/ConversationDetailsResource.php
@@ -25,21 +25,25 @@ class ConversationDetailsResource extends JsonResource
             $senderAvatar = User::find($this->tenant_id)->avatar;
             $receiverName = User::find($this->owner_id)->first_name ." ". User::find($this->owner_id)->last_name;
             $receiverAvatar = User::find($this->owner_id)->avatar;
+            $receiverPhone = User::find($this->owner_id)->phone;
         }else{
             $senderName = User::find($this->owner_id)->first_name ." ". User::find($this->owner_id)->last_name;
             $senderAvatar = User::find($this->owner_id)->avatar;
             $receiverName = User::find($this->tenant_id)->first_name ." ". User::find($this->tenant_id)->last_name;
             $receiverAvatar = User::find($this->tenant_id)->avatar;
+            $receiverPhone = User::find($this->tenant_id)->phone;
         }
         $propertyImage = Property::find($this->property_id)->images()->first()->path;
         $title = Property::find($this->property_id)->description;
         $pricePerNight = Property::find($this->property_id)->price_per_night;
+
     
         return [
             'sender name' => $senderName,
             'sender avatar' => $senderAvatar,
             'receiver name' => $receiverName,
             'receiver avatar' => $receiverAvatar,
+            'receiver phone number' => $receiverPhone,
             'Property information' => [
                 'image' => 'storage/' . $propertyImage,
                 'title' => $title,
